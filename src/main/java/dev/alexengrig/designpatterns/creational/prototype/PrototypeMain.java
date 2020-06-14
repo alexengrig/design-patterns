84e95a88704a60ba44c3568c27a605c4d9478a08
Grig Alex <alexengrigdev@gmail.com>
2020-06-14 13:47:00 +0300

    Prototype: Refactor PrototypeMain

--- src/main/java/dev/alexengrig/designpatterns/creational/prototype/PrototypeMain.java
+++ src/main/java/dev/alexengrig/designpatterns/creational/prototype/PrototypeMain.java
@@ -3,8 +3,15 @@ package dev.alexengrig.designpatterns.creational.prototype;
 public class PrototypeMain {
     public static void main(String[] args) {
         Man bob = new Man("Bob", 170, 60);
-        Man bobClone = bob.clone();
-        System.out.println(bobClone);
-        System.out.println("Is it Bob's clone? " + (bob != bobClone && bob.equals(bobClone) ? "Yes." : "No."));
+        Cat tom = new Cat(bob, "Tom", 2);
+        Cat tomClone = new Cat(tom);
+        System.out.println(tomClone);
+        System.out.println("Is it Tom's clone? " + (isClone(tom, tomClone) ? "Yes." : "No."));
+        Man bobClone = tomClone.getOwner();
+        System.out.println("Is it Bob's clone? " + (isClone(bob, bobClone) ? "Yes." : "No."));
+    }
+
+    private static boolean isClone(Object object, Object clone) {
+        return object != clone && object.equals(clone);
     }
 }
