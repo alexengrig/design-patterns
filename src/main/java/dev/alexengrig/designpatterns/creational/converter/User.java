3d8755af0e591d5470e0c00535dfdeaf90457f5b
Grig Alex <alexengrigdev@gmail.com>
2020-06-17 00:43:51 +0300

    Converter: Create User and UserDto

---
+++ src/main/java/dev/alexengrig/designpatterns/creational/converter/User.java
@@ -0,0 +1,39 @@
+package dev.alexengrig.designpatterns.creational.converter;
+
+public class User {
+    private Long id;
+    private String password;
+    private final String name;
+    private final String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/dev/alexengrig/designpatterns/creational/converter/UserDto.java
@@ -0,0 +1,27 @@
+package dev.alexengrig.designpatterns.creational.converter;
+
+public class UserDto {
+    private final String name;
+    private final String email;
+
+    public UserDto(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
