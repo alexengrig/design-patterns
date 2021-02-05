50fc69f78f284f08c72bb2b67f0954a8776af7ce
Grig Alex <alexengrigdev@gmail.com>
2021-02-06 00:57:16 +0300

    Composite: Add constructor with varargs to Box

--- src/main/java/dev/alexengrig/designpatterns/structural/composite/Box.java
+++ src/main/java/dev/alexengrig/designpatterns/structural/composite/Box.java
@@ -1,5 +1,6 @@
 package dev.alexengrig.designpatterns.structural.composite;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Box implements Priceable {
@@ -7,6 +8,10 @@ public class Box implements Priceable {
 
     public Box(List<? extends Priceable> values) {
         this.values = values;
+    }
+
+    public Box(Priceable... values) {
+        this(Arrays.asList(values));
     }
 
     @Override
--- src/main/java/dev/alexengrig/designpatterns/structural/composite/CompositeMain.java
+++ src/main/java/dev/alexengrig/designpatterns/structural/composite/CompositeMain.java
@@ -1,14 +1,12 @@
 package dev.alexengrig.designpatterns.structural.composite;
 
-import java.util.Arrays;
-
 public class CompositeMain {
     public static void main(String[] args) {
         Goods book = new Goods("Book", 12.5);
         Goods notebook = new Goods("Notebook", 5.5);
-        Box boxWithBookAndNotebook = new Box(Arrays.asList(book, notebook));
+        Box boxWithBookAndNotebook = new Box(book, notebook);
         Goods pen = new Goods("Pen", 1.5);
-        Box box = new Box(Arrays.asList(boxWithBookAndNotebook, pen));
+        Box box = new Box(boxWithBookAndNotebook, pen);
         System.out.println("Total price: " + box.price());
     }
 }
